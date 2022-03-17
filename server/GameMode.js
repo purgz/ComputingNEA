ca4748895459799a04fccf6def6573ae33abf0e9
purgz <86165874+purgz@users.noreply.github.com>
2022-03-17 09:56:18 +0000

    refactoring
    break down of the app.js into 3 smaller folders, one for the game objects and one for the static functions for move calculation. This makes the whole project much easier to read and update.

---
+++ server/GameMode.js
@@ -0,0 +1,397 @@
+const tools = require("./tools");
+
+class GameRoom {
+    //instantiate room with main attributes.
+    constructor(){
+        this.gamestate = GenerateDefaultPosition();
+        this.player1;
+        this.player2;
+        this.playerLegalMoves = [];
+        this.turn = ""; //until 2 users no colour so no one can moves
+        //users initalised with castling rights
+        this.player1LongCastle = true;
+        this.player2LongCastle = true;
+        this.player1ShortCastle = true;
+        this.player2ShortCastle = true;
+        this.enPassant;
+        //elo
+        this.player1rating;
+        this.player2rating;
+        this.player1ExpectedScore;
+        this.player2ExpectedScore;
+        this.player1Score;
+        this.player2Score;
+
+        this.gameType = "regular";
+    }
+
+    GetGameType(){
+        return this.gameType;
+    }
+    //adds users to the game and gives them their session colour variables - spectators do not get colour
+    AddUsers(uname,rating){
+        if (this.player1 && this.player2){
+            return "spectator";
+        }
+        rating = parseInt(rating);
+        //randomly assinging player 1 and 2 colours white or black. spectator does not get a colour
+        if (!(this.player1)){
+            this.player1 = uname;
+            this.player1rating = rating;
+            if (Math.floor(Math.random() * 2) == 1){
+                this.player1Colour = "white";
+            } else {
+                this.player1Colour = "black";
+            }
+            return this.player1Colour;
+        } else {
+            this.player2 = uname;
+            this.player2rating = rating;
+            if (this.player1Colour == "white"){
+                this.player2Colour = "black";
+            } else {
+                this.player2Colour = "white";
+            }
+            this.ExpectedScores();
+            this.turn = "white"; //initlialse the game turns.
+            return this.player2Colour;
+        }
+    }
+
+    //deals with move requests from the current game room
+    UpdateBoard(currentCell,newSquare,playerColour){
+        //adjusts cell and square to match array indexes
+        currentCell -= 1;
+        newSquare -= 1;
+        var opponentColour;
+        
+        //calculate opponent colour
+        if (playerColour == this.player1Colour){
+            opponentColour = this.player2Colour;
+        } else {
+            opponentColour = this.player1Colour;
+        }
+        
+        //spectator cant move and also cant move if not your turn
+        if (playerColour !== this.turn){ console.log("not your turn");return; }
+        if (playerColour[0] !== this.gamestate[currentCell][0]){ console.log("not your piece"); return;}
+
+        //calculate your legal mvoes
+        this.playerLegalMoves = this.LegalMoves(currentCell,playerColour,this.gamestate);
+        
+        //if the move is legal move piece and make old square = to ""
+        if (this.playerLegalMoves.includes(newSquare) && !(this.CheckAfterMove(playerColour,currentCell,newSquare))){
+            //checks for pawn promotion
+            if (this.gamestate[currentCell][1] == "P"){
+                this.Promotion(currentCell,newSquare);
+                if (newSquare == currentCell + 16 || newSquare == currentCell - 16){
+                    //if a user makes a pawn move then the latest square is set to enpassant
+                    this.enPassant = newSquare;
+
+                } else{
+                    this.enPassant = "";
+                }
+            } else{
+                this.enPassant = "";
+            }
+            //enpassant move
+            if (this.gamestate[currentCell][1] == "P"){
+                if (this.gamestate[newSquare]==""){
+                    if (newSquare == currentCell - 9 || newSquare == currentCell + 7){
+                        this.gamestate[currentCell-1] = "";
+                    } else if(newSquare == currentCell + 9 || newSquare == currentCell - 7){
+                        this.gamestate[currentCell+1] = "";
+                    }
+                }
+            }
+            this.gamestate[newSquare] = this.gamestate[currentCell];
+            this.gamestate[currentCell] = "";
+
+        //deals with castling - checks if check after move then if the moves inbetween are in check, then changes players castling status on and off
+        } else if(this.gamestate[currentCell][1] == "K" && !(this.playerLegalMoves.includes(newSquare)) 
+            && !(this.CheckAfterMove(playerColour,currentCell,newSquare))){
+
+            //short castle
+            if (newSquare == currentCell + 2){
+                //if the player has already short castle
+                if (playerColour == this.player1Colour){
+                    if (!(this.player1ShortCastle)){ return; }
+                } else{
+                    if (!(this.player2ShortCastle)){ return; }
+                }
+                //if castling squares are under attack
+                if (this.opponentLegalMoves.includes(currentCell) || this.opponentLegalMoves.includes(currentCell+1)
+                    || this.opponentLegalMoves.includes(currentCell+2)){ return; }
+                    
+            //long castle
+            } else if( newSquare == currentCell - 2){
+                //if the player has already long castle
+                if (playerColour == this.player1Colour){
+                    if(!(this.player1LongCastle)){ return; }
+                } else{
+                    if(!(this.player2LongCastle)){ return; }
+                }
+                if (this.opponentLegalMoves.includes(currentCell) || this.opponentLegalMoves.includes(currentCell-1)
+                    || this.opponentLegalMoves.includes(currentCell-2)){ return; }
+            }
+
+            //if castle is not allowed then return, else pieces are moved and function continues
+            if (!(this.Castle(currentCell,newSquare))){ return; }
+
+        } else {
+            console.log("illegal move");
+            return;
+        }
+       
+        //requires refactoring
+        //alternate the turn when a move is finalised.
+        if (playerColour == this.player1Colour){
+            this.turn = this.player2Colour;
+            //remove castling rights for long and short castle as needed
+            let castlingRights = tools.HasCastled(currentCell,newSquare,this.gamestate,this.player1ShortCastle,this.player1LongCastle,playerColour)
+            this.player1ShortCastle = castlingRights[0];
+            this.player1LongCastle = castlingRights[1];
+        
+        } else {
+            this.turn = this.player1Colour;
+            let castlingRights = tools.HasCastled(currentCell,newSquare,this.gamestate,this.player2ShortCastle,this.player2LongCastle,playerColour);
+            this.player2ShortCastle = castlingRights[0];
+            this.player2LongCastle = castlingRights[1];
+        }
+        //checks whether the game is over by checkmate or stalemate on each move.
+        return this.IsGameOver(opponentColour);
+    }
+
+    LegalMoves(currentCell,playerColour,gamestate){
+        let tempMoves = [];   //array which will return all legal moves
+        let piece = gamestate[currentCell][1];  //selected piece - second letter to tell what piece
+        let yourPieces;  //array of the pieces which are yours and opponents
+        let opPieces; //opponent pieces
+
+        if (playerColour == "white"){
+            yourPieces = whitePieces;
+            opPieces = blackPieces;
+        } else {
+            yourPieces = blackPieces;
+            opPieces = whitePieces;
+        }
+        //call functions for correct piece.
+        switch (piece)
+        {
+            case piece = "R":
+                tempMoves = tools.RookMoves(currentCell,gamestate,yourPieces,opPieces);
+                break;
+            case piece = "B":
+                tempMoves = tools.BishopMoves(currentCell,gamestate,yourPieces,opPieces);
+                break;
+            case piece = "N":
+                tempMoves = tools.KnightMoves(currentCell,gamestate,opPieces);
+                break;
+            case piece = "Q":
+                tempMoves = tools.RookMoves(currentCell,gamestate,yourPieces,opPieces);
+                tempMoves = tempMoves.concat(tools.BishopMoves(currentCell,gamestate,yourPieces,opPieces));
+                break;
+            case piece = "K":
+                tempMoves = tools.KingMoves(currentCell,gamestate,opPieces);
+                break;
+            case piece = "P":
+                tempMoves = tools.PawnMoves(currentCell,gamestate,opPieces,playerColour,this.enPassant)
+                break;  
+            default:
+                break;
+        }
+        
+        return tempMoves;
+    }
+
+    //added opponenet legal moves
+    OpponentLegalMoves(gamestate,opponentColour){
+        
+        this.opponentLegalMoves = [];
+        //calc opponent legal moves
+        for (let i = 0; i < 64; i++) {
+            if (gamestate[i][0] == opponentColour[0]) {
+                this.opponentLegalMoves = this.opponentLegalMoves.concat(this.LegalMoves(i, opponentColour, gamestate));
+            }
+        }
+        return this.opponentLegalMoves;
+    }
+  
+    Promotion(currentCell,newSquare){
+        if (newSquare < 8){
+            this.gamestate[currentCell] = "wQ";
+        } else if (newSquare > 55){
+            this.gamestate[currentCell] = "bQ";
+        }
+    }
+
+    //executes castling
+    Castle(currentCell,newSquare){
+        let newRookSquare;
+        let oldRookSquare;
+        
+        //direction king is moving in
+        if (newSquare == currentCell + 2){
+            //check if squares are empty inbetween king and rook
+            if (this.gamestate[currentCell+1] !== "" || this.gamestate[currentCell+2] !== ""){ return false; }
+            //calculate new and old rook squares
+            newRookSquare = newSquare - 1;
+            oldRookSquare = currentCell + 3;
+            //other direction
+        } else if (newSquare == currentCell -2 ){
+            if (this.gamestate[currentCell-1] !== "" || this.gamestate[currentCell-2] !== ""){ return false; }
+            newRookSquare = newSquare + 1;
+            oldRookSquare = currentCell - 4;
+        } else {
+            //not a legal castle move
+            return false;
+        }
+        //update gamestate with king and rook move
+        this.gamestate[newSquare] = this.gamestate[currentCell]
+        this.gamestate[currentCell] = "";
+        this.gamestate[newRookSquare] = this.gamestate[oldRookSquare] 
+        this.gamestate[oldRookSquare] = "";
+        //castle successfull
+        return true;
+    }
+
+    //elo rating system
+    ExpectedScores(){
+    
+        console.log(this.player1rating,this.player2rating)
+        this.player1ExpectedScore = 1/(1 + 10**((this.player2rating - this.player1rating)/400));
+        this.player2ExpectedScore = 1/(1 + 10**((this.player1rating - this.player2rating)/400));
+        //console.log(this.player1ExpectedScore,this.player2ExpectedScore);
+    }
+    UpdateRatings(name){
+        
+        if (this.player1 == name){
+            var newRating = this.player1rating + 32*(this.player1Score - this.player1ExpectedScore);
+        } else if (this.player2 == name){
+            var newRating = this.player2rating + 32*(this.player2Score - this.player2ExpectedScore);
+        }
+        console.log(newRating);
+        return newRating;
+    }
+    //stops moves when you are in check when given the opponent legal moves
+    CheckAfterMove(playerColour,currentCell,newSquare){
+        //creates a clone of gamestate to test move
+        var tempGamestate = Array.from(this.gamestate);
+        if (tempGamestate[currentCell][1] == "P"){
+            if (tempGamestate[newSquare]==""){
+                if (newSquare == currentCell - 9 || newSquare == currentCell + 7){
+                    tempGamestate[currentCell-1] = "";
+                } else if(newSquare == currentCell + 9 || newSquare == currentCell - 7){
+                    tempGamestate[currentCell+1] = "";
+                }
+            }
+        }
+
+        tempGamestate[newSquare] = tempGamestate[currentCell];
+        tempGamestate[currentCell] = "";
+
+        //if you are still in check after move then move is not allowed.
+        if  (this.KingInCheck(tempGamestate,playerColour)){
+            console.log("your in check");
+            return true;
+        }
+        return false;
+    }
+
+    //function to return whether king is in check for given gamestate
+    KingInCheck(gamestate,playerColour){
+        var king = "";
+        var opColour;
+        //calc colour
+        if (playerColour == "black"){
+            king = "bK";
+            opColour = "white";
+        } else {
+            king = "wK";
+            opColour = "black";
+        }
+        //find king and check opponent legal moves
+        var kingpos = gamestate.indexOf(king);
+        let opponentLegalMoves = this.OpponentLegalMoves(gamestate,opColour);
+    
+        if (opponentLegalMoves.includes(kingpos)){ return true;}
+        return false;
+    }
+
+    //function for detecting checkmate/stalemate
+    IsGameOver(playerColour){
+        var tempGamestate = Array.from(this.gamestate);
+        var checkMate = false; //checkmate or stalemate
+
+        //if king is in check to begin with AND has no moves => checkmate, else just no moves => stalemate
+        if (this.KingInCheck(tempGamestate,playerColour)){
+            checkMate = true;
+        }
+
+        //check each square on the board
+        for (let i = 0; i < 64; i++) {
+            //checks only your pieces
+            if (tempGamestate[i][0] == playerColour[0]) {
+                //calculate legal moves for current piece
+                var newSquares = this.LegalMoves(i, playerColour, tempGamestate);
+                
+                //check each legal move
+                for (let j = 0; j < newSquares.length; j++){
+                    //if the move still ends in a check then go to next move
+                    tempGamestate[newSquares[j]] = tempGamestate[i];
+                    tempGamestate[i] = 0;
+                    if (!(this.KingInCheck(tempGamestate,playerColour))){
+                        //a move that doesnt end in check is found so the game is not over
+                        return;
+                    }
+                    //reset gamestate for next iteration
+                    tempGamestate = Array.from(this.gamestate);
+                }
+            }
+        }
+        //differentiates between checkmate and stalemate
+        if (checkMate){
+            return "Checkmate";
+        } else {
+            return "Stalemate";
+        }
+    }
+}
+//use classes for different game variations
+class TimedGameRoom extends GameRoom {
+    constructor(){
+        super();
+        this.gameType = "Timed";
+        this.player1Time = 30;
+        this.player2Time = 30;
+        this.countdown;
+    }
+    GetGameType(){
+        return this.gameType;
+    }
+}
+
+
+//generates the defualt board layout
+//each index represents a piece, each piece is notated as first letter - colour - second letter - piece
+function GenerateDefaultPosition() {
+    return ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR",
+        "bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP",
+        "", "", "", "", "", "", "", "",
+        "", "", "", "", "", "", "", "",
+        "", "", "", "", "", "", "", "",
+        "", "", "", "", "", "", "", "",
+        "wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP",
+        "wR", "wN", "wB", "wQ", "wK", "wB","wN", "wR"
+    ];
+}
+//consts
+const blackPieces = ["bP","bN","bR","bB","bQ","bK"];
+const whitePieces = ["wP","wN","wR","wB","wQ","wK"];
+
+//could add a number of different variants easily by inheriting GameRoom
+
+module.exports = {
+    GameRoom: GameRoom,
+    TimedGameRoom: TimedGameRoom,
+}
--- server/tools.js
+++ server/tools.js
@@ -29,11 +29,256 @@ function HasCastled(currentCell,newSquare,gamestate,canShortCastle,canLongCastle
     return [canShortCastle,canLongCastle];
 }
 
+//piece functions
+function RookMoves(currentCell,gamestate,yourPieces,opPieces){
+  
+    var args = {
+        currentCell: currentCell,
+        gamestate: gamestate,
+        yourPieces: yourPieces,
+        opPieces: opPieces
+    }
 
+    var moves = [];
+
+    //move options if piece is on edge of board
+    if (rightedges.includes(currentCell)) {
+        moves = moves.concat(GenerateMoves(8,args)).concat(GenerateMoves(-8,args)).concat(GenerateMoves(-1,args));
+        
+    } else if (leftedges.includes(currentCell)) {
+        moves = moves.concat(GenerateMoves(8,args)).concat(GenerateMoves(-8,args)).concat(GenerateMoves(1,args));
+        
+    } else {
+        moves = moves.concat(GenerateMoves(8,args)).concat(GenerateMoves(-8,args)).concat(GenerateMoves(-1,args)).concat(GenerateMoves(1,args));
+    }
+    //console.log(this.moves);
+    return moves;
+}
+
+function BishopMoves(currentCell,gamestate,yourPieces,opPieces){
+    
+    var args = {
+        currentCell: currentCell,
+        gamestate: gamestate,
+        yourPieces: yourPieces,
+        opPieces: opPieces
+    }
+
+    var moves = [];
+
+    //move options if piece is on edge of board
+    if (rightedges.includes(currentCell)) {
+        moves = moves.concat(GenerateMoves(7,args)).concat(GenerateMoves(-9,args));
+       
+    } else if (leftedges.includes(currentCell)) {
+        moves = moves.concat(GenerateMoves(9,args)).concat(GenerateMoves(-7,args));
+      
+    } else {
+        moves = moves.concat(GenerateMoves(7,args)).concat(GenerateMoves(-9,args));
+        moves = moves.concat(GenerateMoves(-7,args)).concat(GenerateMoves(9,args));
+       
+    }
+    //console.log(this.moves);
+    return moves;
+}
+//calculates the rook / bishop / queen moves in a given direction
+function GenerateMoves(dir,args){
+    var moves = [];
+    //iterate 7 times in all the directions
+    for (let i = 1; i < 8; i++){
+        //calculate next square
+        var nextCell = args.currentCell + dir*i;
+    
+        //check if square is in board valid range
+        if (nextCell < 64 && nextCell > -1){
+            //get string id of next cell
+            let piece = args.gamestate[nextCell];
+    
+            //stop moves when they hit edge of board
+            if (!(rightedges.includes(args.currentCell) || leftedges.includes(args.currentCell))) {
+                //if the piece is not on the edge of the board then break once edge of board is met to prevent moves overlapping rows.
+                if (rightedges.includes(nextCell) || leftedges.includes(nextCell)) { i = 7; }; 
+            } 
+
+            if (piece == "") { moves.push(nextCell) } //if square empty allows move
+            else if (args.opPieces.includes(piece)) { moves.push(nextCell); i = 8 } //if opponents piece allows take but no further
+            else if (args.yourPieces.includes(piece)) { i = 8; }  //if yourcolour does not allow the move
+        
+        } 
+    }
+    return moves;
+}
+
+
+//function for knight legal moves
+function KnightMoves(currentCell,gamestate,opPieces){
+
+    var knightMoves = [];  //legal squares to go to
+    var possibleMoves = []; //legal square differences from current squares
+
+    // knight legal moves will changed slightly if on outer two ranks; 
+    if (leftedges.includes(currentCell)){
+        possibleMoves = [-15,-6,10,17];
+    } else if (leftedges2.includes(currentCell)){
+        possibleMoves = [-6,-15,10,17,-17,15]
+    } else if (rightedges.includes(currentCell)){
+        possibleMoves = [-10,-17,6,15]
+    } else if (rightedges2.includes(currentCell)){
+        possibleMoves = [6,15,-10,-17,17,-15]
+    } else {
+        possibleMoves = [6,10,-6,-10,15,17,-15,-17]
+    }
+    //loops each knigtmove and if in board range adds to legal
+    for (let i = 0; i<possibleMoves.length ; i++){
+        var nextCell = currentCell + possibleMoves[i];
+        
+        if (nextCell < 64 && nextCell > -1){
+            let temp = gamestate[nextCell];
+
+            if (temp == "") { knightMoves.push(nextCell) }
+            else if (opPieces.includes(temp)) { knightMoves.push(nextCell) }    
+        }
+    }
+    return knightMoves;
+}
+//function for king legal moves
+function KingMoves(currentCell, gamestate, opPieces){
+    //array for the possible move differences and king legal moves
+    var kingMoves = [];
+    var possibleMoves = [];
+
+    //calculate the possible moves if the king is on either side of the boad
+    if (leftedges.includes(currentCell)){
+        possibleMoves = [1,8,9,-8,-7]
+    } else if (rightedges.includes(currentCell)){
+        possibleMoves = [-1,-8,-9,8,7]
+    } else {
+        possibleMoves = [1,8,9,7,-1,-8,-9,-7]
+    }
+    //iterate through possible moves and add them all to the king moves array
+    for (let i = 0; i<possibleMoves.length ; i++){
+        var nextCell = currentCell + possibleMoves[i];
+        
+        if (nextCell < 64 && nextCell > -1){
+            let temp = gamestate[nextCell];
+
+            //if square is empty or enemy piece, then add to legal moves
+            if (temp == "") { kingMoves.push(nextCell) }
+            else if (opPieces.includes(temp)) { kingMoves.push(nextCell) }    
+        }
+    }
+    return kingMoves;
+}
+
+function PawnMoves(currentCell,gamestate,opPieces,yourColour,enPassant){ 
+    //find direction which your pawn can move.
+    var dir;
+    if (yourColour == "white"){
+        dir = -1;
+    } else {
+        dir = 1;
+    }
+    //initialise possible moves with the standard 1 square pawn move.
+    var possibleMoves = [dir*8];
+    var pawnMoves = [];
+
+    //adds the double square move if pawn is on second rank
+    if ((currentCell > 47 && currentCell < 56 && yourColour == "white") || (currentCell > 7 && currentCell < 16 && yourColour == "black")){
+        possibleMoves.push(dir*16);
+    }
+    
+    //iterate through possible moves and add them all to the king moves array
+    for (let i = 0; i<possibleMoves.length ; i++){
+        var nextCell = currentCell + possibleMoves[i];
+        
+        if (nextCell < 64 && nextCell > -1){
+            let temp = gamestate[nextCell];
+
+            //if square is empty or enemy piece, then add to legal moves
+            if (temp == "") { pawnMoves.push(nextCell) }  
+        }
+    }
+    pawnMoves = pawnMoves.concat((PawnTakeMoves(currentCell,gamestate,opPieces,dir)));
+    
+    pawnMoves.push(EnPassant(enPassant,currentCell,yourColour,gamestate))
+    return pawnMoves;
+}
+//returns the diagonal take squares for selected pawn
+function PawnTakeMoves(currentCell,gamestate,opPieces,dir){
+    var possibleMoves = [];
+    var takeMoves = [];
+    //remove the takes going off edges of the board
+    if (rightedges.includes(currentCell)){
+        if (dir > 0){
+            possibleMoves.push(dir*7);
+        } else {
+            possibleMoves.push(dir*9)
+        }
+    } else if (leftedges.includes(currentCell)){
+        if (dir > 0){
+            possibleMoves.push(dir*9);
+        } else {
+            possibleMoves.push(dir*7)
+        }
+    } else{
+        possibleMoves.push(dir*7);
+        possibleMoves.push(dir*9);
+    }
+    //go through possible diagonal takes, if the pieces are opponents then the take is added to legal moves
+    for (let i = 0; i<possibleMoves.length ; i++){
+        var nextCell = currentCell + possibleMoves[i];
+        
+        if (nextCell < 64 && nextCell > -1){
+            let temp = gamestate[nextCell];
+
+            //if square is empty or enemy piece, then add to legal moves
+            if (opPieces.includes(temp)) { takeMoves.push(nextCell) }  
+        }
+    }
+    return takeMoves;
+}
+//function for calculating en passant moves
+function EnPassant(enPassant,currentCell,yourColour,gamestate){
+    let newSquare;
+    //enpassant is given a value if the previous move was a double pawn move,
+    //if a user is in correct place just after opponent double pawn move then they can enpassant
+
+    //if the selected next move is an enpassant and enpassant is a legal move
+    if (currentCell + 1 == enPassant){
+        //calculate new position
+        if (yourColour == "white"){
+            newSquare = currentCell - 7;
+        } else {
+            newSquare = currentCell+9;
+        }
+    } else if (currentCell - 1 == enPassant){
+        //calc new position
+        if (yourColour == "white"){
+            newSquare =  currentCell-9;
+        } else {
+            newSquare = currentCell+7;
+        }
+    }
+    //if the new square is free then allow
+    if (gamestate[newSquare] == ""){
+        return newSquare;
+    }
+}
+
+
+const leftedges = [0, 8, 16, 24, 32, 40, 48, 56]
+const rightedges = [7, 15, 23, 31, 39, 47, 55, 63]
+
+const leftedges2 = [1, 9, 17, 25, 33, 41, 49, 57]
+const rightedges2 = [6, 14, 22, 30, 38, 46, 54, 62]
 
 module.exports = {
     HasCastled: HasCastled,
-    
+    RookMoves: RookMoves,
+    BishopMoves: BishopMoves,
+    KnightMoves: KnightMoves,
+    KingMoves: KingMoves,
+    PawnMoves: PawnMoves,
 }
 
 //continue refactoring
